e497d2bcb264f7bacef2f22e396203e5600e8e11
Olaitan <olaitanmaryakano@gmail.com>
2026-06-19 10:05:46 +0100

    add mail module

--- src/modules/api/booking/index.ts
+++ src/modules/api/booking/index.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { BookingService } from "./services";
 import { BookingController } from "./controllers";
+import { MailService } from "@/mail/mail.service";
 
 @Module({
-    imports: [],
+    imports: [MailService],
       providers: [BookingService],
       controllers: [BookingController],
       exports: [],
